837ff42a4d007cf5dd255297b7749fe62d51a771
Gavin Lovegrove <gavin@imaging4design.co.nz>
2015-07-30 14:09:59 +1200

    ANZ Config file
    Add new comment for athletes that have passed away

---
+++ application/config/anz_deseased.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*****************************************************/
+// ATHLETES WHO HAVE PASSED AWAY
+/*****************************************************/
+// We do not wish to display these athletes Age in Years / Days on their profile
+// Following array contains athlete IDs of those who have passed away
+$config['passed'] = array(
+	'516570'
+);
