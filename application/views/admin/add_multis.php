e294f4995c6af03f82732b8ac427ac42c3f95bdf
Gavin Lovegrove <gavin@imaging4design.co.nz>
2015-11-26 10:46:56 +1300

    Fix multis admin input

--- application/views/admin/add_multis.php
+++ application/views/admin/add_multis.php
@@ -301,7 +301,7 @@ $('#showEntry').append('<img src="<?php echo base_url() . 'img/loading.gif' ?>" 
 	
 
 
-	var eventID = $('.eventID').val();
+	var eventID = $('#eventID').val();
 	//var eventID = $('input:radio[name=eventID]:checked').val();
 	var ageGroup = $('#ageGroup').val();
 	var competition = $('#competition').val();
